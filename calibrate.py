31f9d284faa5f92b2cdb10cb2c5b96086ddaabd4
Yuxuan Zhang <yuxuan@yuxuanzhang.net>
2023-01-06 11:55:13 -0500

    calibrator ready for use

--- calibrate.py
+++ calibrate.py
@@ -1,9 +1,21 @@
 #!python3
+# System Packages
+import re
+import sys
+import glob
+from pathlib import Path
+from os import mkdir
+from os.path import basename, exists
+# PIP Packages
 import numpy as np
 import cv2 as cv
-import glob
-import sys
-import re
+# Path constants
+CALIB_CHECKER_PATH = "calib_checker/*.png"
+CALIB_WHITE_PATH = "calib_white/*.png"
+RAW_DATA_PATH = "data/raw/*.png"
+SAVE_PATH = Path("data/calibrated/")
+if not exists(SAVE_PATH):
+    mkdir(SAVE_PATH)
 # Checkerboard constants
 GRID = (6, 6)
 H, W, D = 0, 0, 0
@@ -15,11 +27,10 @@ objp[:, :2] = np.mgrid[0:GRID[0], 0:GRID[1]].T.reshape(-1, 2)
 # Arrays to store object points and image points from all the images.
 objpoints = []  # 3d point in real world space
 imgpoints = []  # 2d points in image plane.
-images = list(glob.glob('checker/*.png'))
 WINDOW_NAME = "Original Image"
 cv.namedWindow(WINDOW_NAME, cv.WINDOW_AUTOSIZE)
 cv.startWindowThread()
-for fname in images:
+for fname in list(glob.glob(CALIB_CHECKER_PATH)):
     print("Processing %s" % fname)
     img = cv.cvtColor(cv.imread(fname), cv.COLOR_BGR2GRAY)
     H, W = img.shape
@@ -48,10 +59,10 @@ print(dist)
 newCamMtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (H, W), 1, (H, W))
 dX = 160
 # Prepare brightness map
-images = list(glob.glob('ref_white/*.png'))
+images = list(glob.glob(CALIB_WHITE_PATH))
 bri_map = {}
 for fname in images:
-    ID = fname.replace("ref_white/", "").replace(".png", "")
+    img_ID = basename(fname).replace(".png", "")
     img = cv.cvtColor(cv.imread(fname), cv.COLOR_BGR2GRAY)
     # undistort
     dst = cv.undistort(img, mtx, dist)
@@ -59,20 +70,15 @@ for fname in images:
     h, w = dst.shape
     dst = dst[:, dX:h+dX].astype(np.float32)
     dst = (np.ones(dst.shape) * 255).astype(np.float32) / dst
-    bri_map[ID] = dst
+    bri_map[img_ID] = dst
 # Show corrected checker
-images = list(glob.glob('checker/*.png'))
+images = list(glob.glob(CALIB_CHECKER_PATH))
 WINDOW_NAME = "Corrected Checker"
 cv.namedWindow(WINDOW_NAME, cv.WINDOW_AUTOSIZE)
 cv.startWindowThread()
-for fname in images:
-    img = cv.cvtColor(cv.imread(fname), cv.COLOR_BGR2GRAY)
-    result = re.findall("(?<=checker/)\w+(?=\.png)", fname)
-    if len(result) == 0:
-        print("Error: unable to extract color info from file name '{}'".format(
-            fname), file=sys.stderr)
-        continue
-    img_ID = result[0]
+for img_path in images:
+    img = cv.cvtColor(cv.imread(img_path), cv.COLOR_BGR2GRAY)
+    img_ID = basename(img_path).replace('.png', '')
     # undistort
     dst = cv.undistort(img, mtx, dist)
     # crop the image according to AOI
@@ -80,27 +86,25 @@ for fname in images:
     _dst = dst[:, dX:h+dX]
     dst = _dst.astype(np.float32) * bri_map[img_ID]
     dst = np.minimum(dst, np.ones(dst.shape) * 255).astype(np.uint8)
-    print(np.max(dst), np.min(dst))
     # Show result
     BAR = np.zeros((h, 20))
     cv.imshow(WINDOW_NAME, np.concatenate(
         [img[0:h, dX:h+dX], BAR, _dst, BAR, dst], axis=1).astype(np.uint8))
     cv.setWindowProperty(WINDOW_NAME, cv.WND_PROP_TOPMOST, 1)
-    if (cv.waitKey(0) != 13):
-        sys.exit(0)
     break
 # Show corrected images
 WINDOW_NAME = "Corrected Data"
 cv.namedWindow(WINDOW_NAME, cv.WINDOW_AUTOSIZE)
 cv.startWindowThread()
-images = list(glob.glob('data/*.png'))
-for fname in images:
-    print("Processing %s" % fname)
-    img = cv.cvtColor(cv.imread(fname), cv.COLOR_BGR2GRAY)
-    result = re.findall("(?<=_)\w+(?=\.png)", fname)
+images = list(glob.glob(RAW_DATA_PATH))
+for img_path in images:
+    name = basename(img_path).replace('.png', '')
+    print("Processing %s" % name)
+    img = cv.cvtColor(cv.imread(img_path), cv.COLOR_BGR2GRAY)
+    result = re.findall("(?<=_)\w+$", name)
     if len(result) == 0:
         print("Error: unable to extract color info from file name '{}'".format(
-            fname), file=sys.stderr)
+            name), file=sys.stderr)
         continue
     img_ID = result[0]
     # undistort
@@ -110,11 +114,12 @@ for fname in images:
     dst = dst[:, dX:h+dX]
     dst = dst.astype(np.float32) * bri_map[img_ID]
     dst = np.minimum(dst, np.ones(dst.shape) * 255).astype(np.uint8)
-    print(np.max(dst), np.min(dst))
     # Show result
     cv.imshow(WINDOW_NAME, dst)
     cv.setWindowProperty(WINDOW_NAME, cv.WND_PROP_TOPMOST, 1)
-    cv.imwrite(fname.replace("data/", "results/"), dst)
+    save_path = str(SAVE_PATH / (name + ".png"))
+    cv.imwrite(save_path, dst)
+    print("Saved to " + save_path)
     cv.waitKey(10)
 # Wait until finished
 # cv.destroyAllWindows()
